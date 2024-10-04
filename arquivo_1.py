e0d8d824f1950253630b0da22b4989b3ae371082
Ruan55 <114105714+Ruan55@users.noreply.github.com>
2024-10-04 19:39:13 +0000

    arquivo_1

---
+++ arquivo_1.py
@@ -0,0 +1,13 @@
+print("Ruan Cruz Santos")
+
+
+
+print("Pontos principais do documento de requisitos: "
+"\nIntegração com Mapas: "
+" O sistema deverá ser integrado a serviços de mapas (como Google Maps, OpenStreetMap, etc.) para exibir com precisão a localização dos pontos de coleta de lixo eletrônico. A integração permitirá que os usuários vejam os pontos mais próximos e recebam rotas até esses locais, garantindo uma experiência prática e eficiente. "
+"\nExibição de Pontos de Coleta de Lixo Eletrônico: "
+" O aplicativo será capaz de mostrar em tempo real os pontos de coleta de lixo eletrônico em uma determinada região. Esses pontos poderão ser filtrados por critérios como tipo de lixo aceito, horário de funcionamento e distância do usuário, melhorando a usabilidade e promovendo uma melhor gestão de resíduos. "
+"\nModelo de Lucro: "
+" O documento também deve detalhar como o aplicativo gerará receita. O lucro pode vir de diferentes fontes, como parcerias com empresas de reciclagem que pagam pela divulgação de seus pontos de coleta ou por uma porcentagem de venda de equipamentos de coleta recicláveis. Alternativamente, o aplicativo pode oferecer anúncios ou pacotes premium para empresas que queiram destacar seus pontos de coleta. "
+"\nOutras Funcionalidades: "
+" O aplicativo poderá oferecer recursos adicionais, como a possibilidade de alertar os usuários sobre a necessidade de descarte de eletrônicos, fornecer dicas de reciclagem e promover campanhas de conscientização ambiental. Além disso, poderá permitir a contribuição dos próprios usuários na adição de novos pontos de coleta, com verificações de qualidade feitas pela equipe.")
